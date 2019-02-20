ddf459db2f8335661959debb1cb9abd5aad00673
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-02-20 18:08:04 +0300

    add example content

---
+++ CS/DXApplication1/ViewerForm1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DXApplication1
+{
+    partial class ViewerForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardBarAndDockingController1 = new DevExpress.DashboardWin.Native.DashboardBarAndDockingController(this.components);
+            this.dashboardViewer = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarAndDockingController1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardBarAndDockingController1
+            // 
+            this.dashboardBarAndDockingController1.PropertiesBar.AllowLinkLighting = false;
+            this.dashboardBarAndDockingController1.PropertiesDocking.ViewStyle = DevExpress.XtraBars.Docking2010.Views.DockingViewStyle.Classic;
+            // 
+            // dashboardViewer
+            // 
+            this.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.dashboardViewer.Appearance.Options.UseBackColor = true;
+            this.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer.Location = new System.Drawing.Point(0, 0);
+            this.dashboardViewer.Name = "dashboardViewer";
+            this.dashboardViewer.Size = new System.Drawing.Size(1198, 599);
+            this.dashboardViewer.TabIndex = 0;
+            // 
+            // ViewerForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 599);
+            this.Controls.Add(this.dashboardViewer);
+            this.Name = "ViewerForm1";
+            this.Text = "Dashboard Designer";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardBarAndDockingController1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.Native.DashboardBarAndDockingController dashboardBarAndDockingController1;
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer;
+    }
+}
---
+++ CS/DXApplication1/ViewerForm1.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DataAccess.Excel;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1
+{
+    public partial class ViewerForm1 : XtraForm
+    {
+        public ViewerForm1()
+        {
+            InitializeComponent();
+            
+            dashboardViewer.Dashboard = new Dashboard();
+
+            // Creates a data source and adds it to the dashboard data source collection.
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource();
+            excelDataSource = CreateExcelDataSource();
+            dashboardViewer.Dashboard.DataSources.Add(excelDataSource);
+
+            // Creates a card dashboard item with the specified data source 
+            // and adds it to the Items collection to display within the dashboard.
+            CardDashboardItem cards = CreateCards(excelDataSource);
+            dashboardViewer.Dashboard.Items.Add(cards);
+
+            // Reloads data in the data sources.
+            dashboardViewer.ReloadData();
+        }
+        private CardDashboardItem CreateCards(DashboardExcelDataSource dataSource)
+        {
+            // Creates a card dashboard item and specifies its data source.
+            CardDashboardItem cards = new CardDashboardItem();
+            cards.DataSource = dataSource;
+
+            // Creates the Card object with measures that provide data for calculating actual and target
+            // values, and then adds this object to the Cards collection of the card dashboard item.
+            Card card = new Card();
+            card.ActualValue = new Measure("RevenueQTD (Sum)");
+            card.TargetValue = new Measure("RevenueQTDTarget (Sum)");
+            cards.Cards.Add(card);
+
+            // Specifies dimensions that provides data for a card dashboard item series.
+            cards.SeriesDimensions.Add(new Dimension("Category"));
+            cards.SeriesDimensions.Add(new Dimension("Product"));
+            cards.InteractivityOptions.IsDrillDownEnabled = true;
+
+            return cards;
+        }
+        public DashboardExcelDataSource CreateExcelDataSource()
+        {
+            // Generates the Excel Data Source.
+            DashboardExcelDataSource excelDataSource = new DashboardExcelDataSource();
+            excelDataSource.FileName = @"Data\Sales.xlsx";
+            ExcelWorksheetSettings worksheetSettings = new ExcelWorksheetSettings("Sheet1", "A1:I4166");
+            excelDataSource.SourceOptions = new ExcelSourceOptions(worksheetSettings);
+            excelDataSource.Fill();
+
+            return excelDataSource;
+        }
+    }
+    
+}
