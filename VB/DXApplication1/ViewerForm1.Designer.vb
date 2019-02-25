ff6864fe749a27bd3480440654f895fb8b48797a
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-02-25 17:07:47 +0300

    add vb project

---
+++ VB/DXApplication1/ViewerForm1.Designer.vb
@@ -0,0 +1,69 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace DXApplication1
+	Partial Public Class ViewerForm1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Me.dashboardBarAndDockingController1 = New DevExpress.DashboardWin.Native.DashboardBarAndDockingController(Me.components)
+			Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+			CType(Me.dashboardBarAndDockingController1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' dashboardBarAndDockingController1
+			' 
+			Me.dashboardBarAndDockingController1.PropertiesBar.AllowLinkLighting = False
+			Me.dashboardBarAndDockingController1.PropertiesDocking.ViewStyle = DevExpress.XtraBars.Docking2010.Views.DockingViewStyle.Classic
+			' 
+			' dashboardViewer
+			' 
+			Me.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt(Fix((CByte(210))))), (CInt(Fix((CByte(210))))), (CInt(Fix((CByte(210))))))
+			Me.dashboardViewer.Appearance.Options.UseBackColor = True
+			Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
+			Me.dashboardViewer.Name = "dashboardViewer"
+			Me.dashboardViewer.Size = New System.Drawing.Size(1198, 599)
+			Me.dashboardViewer.TabIndex = 0
+			' 
+			' ViewerForm1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(1198, 599)
+			Me.Controls.Add(Me.dashboardViewer)
+			Me.Name = "ViewerForm1"
+			Me.Text = "Dashboard Viewer"
+			CType(Me.dashboardBarAndDockingController1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private dashboardBarAndDockingController1 As DevExpress.DashboardWin.Native.DashboardBarAndDockingController
+		Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
+	End Class
+End Namespace
---
+++ VB/DXApplication1/ViewerForm1.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DataAccess.Excel
+Imports DevExpress.XtraEditors
+
+Namespace DXApplication1
+	Partial Public Class ViewerForm1
+		Inherits XtraForm
+		Public Sub New()
+			InitializeComponent()
+
+			dashboardViewer.Dashboard = New Dashboard()
+
+			' Creates a data source and adds it to the dashboard data source collection.
+			Dim excelDataSource As New DashboardExcelDataSource()
+			excelDataSource = CreateExcelDataSource()
+			dashboardViewer.Dashboard.DataSources.Add(excelDataSource)
+
+			' Creates a card dashboard item with the specified data source 
+			' and adds it to the Items collection to display within the dashboard.
+			Dim cards As CardDashboardItem = CreateCards(excelDataSource)
+			dashboardViewer.Dashboard.Items.Add(cards)
+
+			' Reloads data in the data sources.
+			dashboardViewer.ReloadData()
+		End Sub
+		Private Function CreateCards(ByVal dataSource As DashboardExcelDataSource) As CardDashboardItem
+			' Creates a card dashboard item and specifies its data source.
+			Dim cards As New CardDashboardItem()
+			cards.DataSource = dataSource
+
+			' Creates the Card object with measures that provide data for calculating actual and target
+			' values, and then adds this object to the Cards collection of the card dashboard item.
+			Dim card As New Card()
+			card.ActualValue = New Measure("RevenueQTD (Sum)")
+			card.TargetValue = New Measure("RevenueQTDTarget (Sum)")
+			cards.Cards.Add(card)
+
+			' Specifies dimensions that provides data for a card dashboard item series.
+			cards.SeriesDimensions.Add(New Dimension("Category"))
+			cards.SeriesDimensions.Add(New Dimension("Product"))
+			cards.InteractivityOptions.IsDrillDownEnabled = True
+
+			Return cards
+		End Function
+		Public Function CreateExcelDataSource() As DashboardExcelDataSource
+			' Generates the Excel Data Source.
+			Dim excelDataSource As New DashboardExcelDataSource()
+			excelDataSource.FileName = "Data\Sales.xlsx"
+			Dim worksheetSettings As New ExcelWorksheetSettings("Sheet1", "A1:I4166")
+			excelDataSource.SourceOptions = New ExcelSourceOptions(worksheetSettings)
+			excelDataSource.Fill()
+
+			Return excelDataSource
+		End Function
+	End Class
+
+End Namespace
